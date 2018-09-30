73c6dac94604026641c0fa651ea49fc74cf3c1ef
Shreejit <shreejit955@gmail.com>
2018-09-30 09:43:08 +0545

    changed ast1

--- js-experiments/ast1/js/wave.js
+++ js-experiments/ast1/js/wave.js
@@ -1,49 +1,41 @@
 
 
-//array storeing all the pattern needed
-var arr=["*","**","***","****","*****","******"];
-
-//index that specified the index to be printed
-var index=-1;
-
-//flag if 1 then increase else decrease index
+var pattern="*******";
+var index=1;
 var increase=1;
-
-//reference to stop setInterval
 var ref;
-
 
 function animate()
 {
-    ref=setInterval(function()
-    {
+    ref=setInterval(drawPattern,50);
+}
+
+function drawPattern()
+{
+    console.log(pattern.substring(0,index));
     
-                if(increase)
-                {
-                    index++;
-                    if(index>5){
-                        increase=0;
-                        index=index-2;       //this is to decrease 6 to 4 not 5
-                    }
-                }
-                else
-                {
-                    index--;
-                    if(index<0)
-                    {
-                        increase=1;
-                        index=index+2;      //this is to increase -1 to 1 not 0
-                    }
-                }
-                //printing the pattern
-                console.log(arr[index]);
-    },100);
-    return;
+    if(increase)
+    {
+        index++;
+        if(index>=pattern.length)
+        {
+            index=index-2;
+            increase=0;
+        }
+    }
+    else{
+        index--;
+        if(index<1)
+        {
+            index=index+2;
+            increase=1;
+        }
+    }
+   
 }
 
 
-
-//stoping the animation
-function animateStop(){
+function animationStop()
+{
     clearInterval(ref);
 }
